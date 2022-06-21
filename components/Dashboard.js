2e0185eb6ba1f6484fb88a85b0e7cf869584651d
Souvik Nath <souviknsl07@gmail.com>
2022-06-21 21:45:01 +0530

    slide3 and slide5 created

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,7 +1,68 @@
+import Image from "next/image";
 import React from "react";
 
 const Dashboard = () => {
-  return <div className="mx-auto  my-[100px] w-[87%]">Dashboard</div>;
+  return (
+    <div className="mx-auto flex flex-col justify-center items-center bg-slate-100 overflow-hidden shadow-lg  my-[150px] w-[87%] pt-10 rounded-2xl">
+      <h1 className="text-4xl font-bold">What we have</h1>
+      <div className="flex items-center space-x-20">
+        <Image
+          src="/assets/engineer.png"
+          height={350}
+          width={350}
+          objectFit="contain"
+        />
+
+        <div className="bg-slate-200  rounded-xl flex flex-col py-10 px-5 border border-blue-500 h-16 justify-center items-center">
+          <span className="text-2xl">3+</span>
+          <p className="text-xs">years of experience</p>
+        </div>
+        <div className="bg-slate-200  rounded-xl flex flex-col py-10 px-5 border border-blue-500 h-16 justify-center items-center">
+          <span className="text-2xl">18+</span>
+          <p className="text-xs">world wide clients</p>
+        </div>
+        <div className="bg-slate-200  rounded-xl flex flex-col py-10 px-5 border border-blue-500 h-16 justify-center items-center">
+          <span className="text-2xl">25+</span>
+          <p className="text-xs">projects completed</p>
+        </div>
+      </div>
+      <h1 className="text-3xl font-semibold">Our Clients</h1>
+      <div className="flex space-x-28">
+        <div>
+          <Image
+            src="/assets/go-kart.png"
+            height={130}
+            width={130}
+            objectFit="contain"
+          />
+        </div>
+        <div>
+          <Image
+            src="/assets/spotmies-logo.png"
+            height={130}
+            width={130}
+            objectFit="contain"
+          />
+        </div>
+        <div>
+          <Image
+            src="/assets/go-kart.png"
+            height={130}
+            width={130}
+            objectFit="contain"
+          />
+        </div>
+        <div>
+          <Image
+            src="/assets/spotmies-logo.png"
+            height={130}
+            width={130}
+            objectFit="contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- components/Technologies.js
+++ components/Technologies.js
@@ -1,9 +1,69 @@
 import React from "react";
+import Image from "next/image";
+
+import * as Constants from "./Constants";
 
 const Technologies = () => {
   return (
     <div className="mx-auto justify-between flex my-[100px] w-[87%]">
-      Technologies
+      <div>
+        <h2 className="text-5xl flex flex-col font-bold space-y-2">
+          <span className="text-blue-400">Technologies</span>
+          <span>we used</span>
+        </h2>
+        <p className="w-[60%] mt-10 text-gray-500 text-lg leading-tight">
+          {Constants.DESCRIPTION}
+        </p>
+      </div>
+      <div className="flex flex-col space-y-6 mr-24">
+        <div className="flex space-x-6">
+          <div className="cursor-pointer scale-125 max-w-sm py-5 px-3 w-32 bg-slate-100 rounded-lg overflow-hidden shadow-lg flex flex-col justify-center items-center">
+            <Image
+              className="border rounded-full bg-white"
+              src="/assets/spotmies_logo1.png"
+              height={80}
+              width={80}
+              ObjectFit="contain"
+            />
+            <div className="text-md text-blue-500 font-semibold mt-5">
+              React js
+            </div>
+          </div>
+          <div className="cursor-pointer transition hover:text-blue-500 hover:scale-125 duration-500 ease-in-out max-w-sm py-5 px-3 w-32 h-40 bg-slate-100 rounded-lg overflow-hidden shadow-lg flex flex-col justify-center items-center">
+            <Image
+              className="border rounded-full bg-white"
+              src="/assets/spotmies_logo1.png"
+              height={80}
+              width={80}
+              ObjectFit="contain"
+            />
+            <div className="text-md font-semibold mt-5">React js</div>
+          </div>
+        </div>
+
+        <div className="flex space-x-6">
+          <div className="cursor-pointer mt-6 transition hover:text-blue-500 hover:scale-125 duration-500 ease-in-out max-w-sm py-5 px-3 w-32 bg-slate-100 rounded-lg overflow-hidden shadow-lg flex flex-col justify-center items-center">
+            <Image
+              className="border rounded-full bg-white"
+              src="/assets/spotmies_logo1.png"
+              height={80}
+              width={80}
+              ObjectFit="contain"
+            />
+            <div className="text-md font-semibold mt-5">React js</div>
+          </div>
+          <div className="cursor-pointer transition hover:text-blue-500 hover:scale-125 duration-500 ease-in-out max-w-sm py-5 px-3 w-32 h-40 bg-slate-100 rounded-lg overflow-hidden shadow-lg flex flex-col justify-center items-center">
+            <Image
+              className="border rounded-full bg-white"
+              src="/assets/spotmies_logo1.png"
+              height={80}
+              width={80}
+              ObjectFit="contain"
+            />
+            <div className="text-md font-semibold mt-5 ">React js</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
