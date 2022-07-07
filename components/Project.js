77e1119c531f4ee0d6a8a5411ccca6e27627f9ab
sekhar javvadi spotmies <63062130+sekharjavvadi@users.noreply.github.com>
2022-07-07 23:24:00 +0530

    Revert "testimonial"

--- components/Project.js
+++ components/Project.js
@@ -1,20 +1,4 @@
 import React from "react";
-import Container from "./Container";
-import Container2 from "./Container2";
-import Container3 from "./Container3";
-import Container4 from "./Container4";
-const boxes = {
-  display: "flex",
-};
-const box1 = {
-  flexDirection: "row",
-
-  margin: "60px 60px ",
-};
-const box2 = {
-  flexDirection: "row",
-  marginRight: "-190px",
-};
 
 const Project = ({ projectRef }) => {
   return (
@@ -22,10 +6,8 @@ const Project = ({ projectRef }) => {
       ref={projectRef}
       className="mx-auto justify-between flex my-[100px] w-[84%]"
     >
-      <div className="ml-[40px] mt-[30px]">
-        <div className="w-[7%]  bg-gray-300 h-[7%] rounded-full ml-[-47px]"></div>
-
-        <h2 className="text-5xl font-bold mt-[-27px]  ">
+      <div>
+        <h2 className="text-5xl font-bold mt-[120px]">
           Our <span className="text-blue-400">Projects</span>
         </h2>
         <p className="w-[45%] mt-[60px] text-gray-500 text-lg">
@@ -33,28 +15,9 @@ const Project = ({ projectRef }) => {
           odio, cum quam maxime, veniam dolor tempore ut rerum magni sint,
           incidunt alias error autem voluptatibus natus excepturi eos! Cum.
         </p>
-        <div className="mt-7">
-          <a
-            href="#"
-            className="my-5 border-blue-300 border-solid border-2 rounded-full p-2 pr-10 pl-10 text-sky-600  "
-          >
-            View All
-          </a>
-        </div>
-        <div className="w-[3%] mt-[-34px]  ml-[30%] bg-gray-300 h-[3%] rounded-full"></div>
-
-        <div className="w-[10%] mt-[90px]  ml-[-77px] bg-gray-300 h-[10%] rounded-full"></div>
+        <p className="my-5">Button</p>
       </div>
-      <div style={boxes}>
-        <div style={box1}>
-          <Container></Container>
-          <Container2></Container2>
-        </div>
-        <div style={box2}>
-          <Container4></Container4>
-          <Container3></Container3>
-        </div>
-      </div>
+      <div>Cards</div>
     </div>
   );
 };
