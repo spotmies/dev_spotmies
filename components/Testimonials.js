8fb71e4a4d3d4e85b0778d6ae7e28117cfc254be
sekharjavvadi <sekharatece@gmail.com>
2022-08-24 09:38:26 +0530

    auto expand while scrolling horizontal code added

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -3,7 +3,22 @@ import React, { useEffect, useState } from "react";
 
 const people = [
   {
-    image: "/assets/gamer.png",
+    image:
+      "https://www.clipartmax.com/png/middle/257-2572603_user-man-social-avatar-profile-icon-man-avatar-in-circle.png",
+    description: "lorem ipsum dolor sit amet, consectetur adip",
+    name: "Souvik1",
+    designation: "Developer",
+  },
+  {
+    image:
+      "https://www.clipartmax.com/png/middle/257-2572603_user-man-social-avatar-profile-icon-man-avatar-in-circle.png",
+    description: "lorem ipsum dolor sit amet, consectetur adip",
+    name: "Souvik1",
+    designation: "Developer",
+  },
+  {
+    image:
+      "https://www.clipartmax.com/png/middle/257-2572603_user-man-social-avatar-profile-icon-man-avatar-in-circle.png",
     description: "lorem ipsum dolor sit amet, consectetur adip",
     name: "Souvik1",
     designation: "Developer",
@@ -52,19 +67,8 @@ const people = [
   },
   {
     image: "/assets/gamer.png",
-    description: "lorem ipsum dolor sit amet, consectetur adip",
-    name: "Souvik",
-    designation: "Developer",
-  },
-  {
-    image: "/assets/gamer.png",
-    description: "lorem ipsum dolor sit amet, consectetur adip",
-    name: "Souvik",
-    designation: "Developer",
-  },
-  {
-    image: "/assets/gamer.png",
-    description: "lorem ipsum dolor sit amet, consectetur adip",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A praesentium eligendi aperiam excepturi reiciendis possimus harum consequatur placeat alias quos, iusto aliquam fugit modi quisquam labore accusamus, tempora, ratione ad.",
     name: "Souvik",
     designation: "Developer",
   },
@@ -79,6 +83,7 @@ const people = [
 const Testimonials = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [paused, setPaused] = useState(false);
+  const [expandIndex, setexpandIndex] = useState(5);
 
   const updateIndex = (newIndex) => {
     if (newIndex < 0) {
@@ -86,6 +91,10 @@ const Testimonials = () => {
     } else if (newIndex >= people.length - 7) {
       newIndex = 0;
     }
+    console.log(newIndex);
+    setTimeout(() => {
+      setexpandIndex(newIndex + 5);
+    }, 300);
 
     setActiveIndex(newIndex);
   };
@@ -112,20 +121,23 @@ const Testimonials = () => {
           className="flex items-center justify-center space-x-24 ml-[15rem] overflow-hidden transition transform duration-1000 h-[100%] ease-linear"
           style={{ transform: `translateX(-${activeIndex * 10}%)` }}
         >
-          {people.map((p) => (
+          {people.map((p, index) => (
             <div
-              onMouseEnter={() => setPaused(true)}
+              onMouseEnter={() => {
+                setPaused(true);
+                setexpandIndex(index);
+              }}
               onMouseLeave={() => setPaused(false)}
               className={`cursor-pointer transition hover:scale-125 hover:flex flex-col items-center duration-1000 ease-in-out group ${
-                p === people[5] || p === people[8] ? "scale-125 flex" : ""
+                index === expandIndex ? "scale-125 flex" : ""
               } `}
             >
               <div
                 className={`group-hover:border-[1.5px]  border-blue-400 rounded-full min-h-[80px] min-w-[85px] max-w-[85px] ${
-                  p === people[5] || p === people[8] ? "border-[1.5px]" : ""
+                  index === expandIndex ? "border-[1.5px]" : ""
                 } `}
               >
-                <Image
+                <img
                   className=""
                   src={p.image}
                   height={80}
@@ -134,9 +146,9 @@ const Testimonials = () => {
                 />
               </div>
               <div
-                className={`flex-col transition duration-800 ease-in-out group-hover:flex mt-5 w-[150px] ${
-                  p === people[5] || p === people[8] ? "flex" : "hidden"
-                } `}
+                className={`flex-col transition duration-800 ease-in-out group-hover:flex mt-5 w-[150px] 
+                ${index === expandIndex ? "flex" : "hidden"} 
+                `}
               >
                 <p className="text-xs text-slate-400">{p.description}</p>
                 <p className="mt-3 font-semibold ml-24 text-xs">- {p.name}</p>
