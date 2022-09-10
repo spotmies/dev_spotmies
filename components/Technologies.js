315d4ac03a6670875370d1c3ea992f0444c5cfd6
Souvik Nath <souviknsl07@gmail.com>
2022-09-10 10:30:27 +0530

    implemented the image border appearing on hover and disappearing after that

--- components/Technologies.js
+++ components/Technologies.js
@@ -5,13 +5,16 @@ import * as Constants from "./Constants";
 
 const Technologies = () => {
   const card1Ref = useRef(null);
+  const card1ImageRef = useRef(null);
 
   // useEffect(() => {
 
   // }, []);
   const reduceSize = () => {
     const card1 = card1Ref.current;
+    const card1Image = card1ImageRef.current;
     card1.classList.remove("scale-125");
+    card1Image.classList.remove("border");
   };
 
   return (
@@ -76,9 +79,12 @@ const Technologies = () => {
         <div className="z-10 flex space-x-8">
           <div
             ref={card1Ref}
-            className="cursor-pointer transition duration-500 ease-in-out hover:scale-125 scale-125 max-w-sm py-5 px-3 w-40  rounded-2xl overflow-hidden shadow-lg flex flex-col justify-center items-center bg-gradient-to-t from-blue-50 to-white"
+            className="cursor-pointer transition duration-500 ease-in-out hover:scale-125 scale-125 max-w-sm py-5 px-3 w-40  rounded-2xl overflow-hidden shadow-lg flex flex-col justify-center items-center bg-gradient-to-t from-blue-50 to-white group"
           >
-            <div className="border border-blue-300 rounded-full bg-gradient-to-l p-2 from-slate-200">
+            <div
+              ref={card1ImageRef}
+              className="border group-hover:border border-blue-300 rounded-full bg-gradient-to-l p-2 from-slate-200"
+            >
               <Image
                 className=""
                 src="/assets/flutter.png"
@@ -94,15 +100,16 @@ const Technologies = () => {
           </div>
           <div
             onMouseEnter={reduceSize}
-            className={`cursor-pointer transition hover:text-blue-500 hover:scale-125 duration-500 ease-in-out max-w-sm pt-3 pb-5 px-3 w-36 h-40 bg-gradient-to-t from-blue-50 to-white rounded-2xl overflow-hidden shadow-lg flex flex-col justify-center items-center `}
+            className="cursor-pointer transition hover:text-blue-500 hover:scale-125 duration-500 ease-in-out max-w-sm pt-3 pb-5 px-3 w-36 h-40 bg-gradient-to-t from-blue-50 to-white rounded-2xl overflow-hidden shadow-lg flex flex-col justify-center items-center group"
           >
-            <Image
-              className=" rounded-full bg-gradient-to-bl from-slate-200 p-2"
-              src="/assets/react.png"
-              height={80}
-              width={80}
-              objectFit="contain"
-            />
+            <div className="group-hover:border border-blue-300 rounded-full bg-gradient-to-b from-slate-200 p-2">
+              <Image
+                src="/assets/react.png"
+                height={70}
+                width={75}
+                objectFit="cover"
+              />
+            </div>
             <div className="text-xs font-semibold mt-5">React js</div>
           </div>
         </div>
@@ -110,22 +117,24 @@ const Technologies = () => {
         <div className="z-10 flex space-x-6 ml-8">
           <div
             onMouseEnter={reduceSize}
-            className="cursor-pointer mt-6 transition hover:text-blue-500 hover:scale-125 duration-500 ease-in-out max-w-sm pt-3 pb-5 px-3 w-36 h-40 rounded-2xl overflow-hidden shadow-lg flex flex-col justify-center items-center bg-white"
+            className="cursor-pointer mt-6 transition hover:text-blue-500 hover:scale-125 duration-500 ease-in-out max-w-sm pt-3 pb-5 px-3 w-36 h-40 rounded-2xl overflow-hidden shadow-lg flex flex-col justify-center items-center bg-white group"
           >
-            <Image
-              className=" rounded-full bg-gradient-to-b from-slate-200 p-2"
-              src="/assets/react.png"
-              height={80}
-              width={80}
-              objectFit="contain"
-            />
+            <div className="group-hover:border border-blue-300 rounded-full bg-gradient-to-b from-slate-200 p-2">
+              <Image
+                src="/assets/react.png"
+                height={70}
+                width={75}
+                objectFit="cover"
+              />
+            </div>
+
             <div className="text-xs font-semibold mt-5">React js</div>
           </div>
           <div
             onMouseEnter={reduceSize}
-            className="cursor-pointer transition hover:text-blue-500 hover:scale-125 duration-500 ease-in-out max-w-sm pt-6 pb-6 px-3 w-36 h-40 rounded-2xl overflow-hidden shadow-lg flex flex-col justify-center items-center bg-white"
+            className="cursor-pointer transition hover:text-blue-500 hover:scale-125 duration-500 ease-in-out max-w-sm pt-6 pb-6 px-3 w-36 h-40 rounded-2xl overflow-hidden shadow-lg flex flex-col justify-center items-center bg-white group"
           >
-            <div className="rounded-full bg-gradient-to-b from-slate-200 p-2">
+            <div className="group-hover:border border-blue-300 rounded-full bg-gradient-to-b from-slate-200 p-2">
               <Image
                 src="/assets/adobe.png"
                 height={70}
